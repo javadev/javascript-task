e423bfeca4c869fc9929ccd9cbd58c77806e9344
javadev <javadev75@gmail.com>
2017-05-17 09:21:06 +0300

    Add generate day function.

---
+++ generate-day/lib/generateDay.js
@@ -0,0 +1,14 @@
+function generateDay (n) {
+  if (n === undefined || n === null) {
+    throw new Error('n is null or undefined');
+  }
+  if (n === 1) {
+    return "день";
+  } else if (n === 2 || n === 3 || n === 4) {
+    return "дня";
+  }
+  return "дней";
+}
+module.exports = {
+  generateDay: generateDay
+};
---
+++ generate-day/test/generateDay.spec.js
@@ -0,0 +1,24 @@
+var gd = require('../lib/generateDay.js');
+var chai         = require('chai');
+var expect       = chai.expect;
+/* jshint camelcase: false */
+describe('generateDay', function () {
+  describe('generateDay', function () {
+    it('should correct for 1 day', function () {
+       expect(gd.generateDay(1)).to.eql('день');
+    });
+    it('should correct for 3 days', function () {
+       expect(gd.generateDay(3)).to.eql('дня');
+    });
+    it('should correct for 14 days', function () {
+       expect(gd.generateDay(14)).to.eql('дней');
+    });
+    it('should throws exception if second parameter is null', function () {
+       expect(gd.generateDay.bind(null)).to.throw(Error);
+    });
+    it('should throws exception if first parameter is undefined',
+    function () {
+       expect(gd.generateDay.bind(undefined)).to.throw(Error);
+    });
+  });
+});
