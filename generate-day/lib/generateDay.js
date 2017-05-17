b8382074626c86d81b951b0ec0e88b7f3354d5fe
javadev <javadev75@gmail.com>
2017-05-17 09:51:38 +0300

    Add support for more than 20 days.

--- generate-day/lib/generateDay.js
+++ generate-day/lib/generateDay.js
@@ -2,6 +2,9 @@ function generateDay (n) {
   if (n === undefined || n === null) {
     throw new Error('n is null or undefined');
   }
+  if (n > 20) {
+    n = n % 10;
+  }
   if (n === 1) {
     return "день";
   } else if (n === 2 || n === 3 || n === 4) {
--- generate-day/test/generateDay.spec.js
+++ generate-day/test/generateDay.spec.js
@@ -13,6 +13,9 @@ describe('generateDay', function () {
     it('should correct for 14 days', function () {
        expect(gd.generateDay(14)).to.eql('дней');
     });
+    it('should correct for 24 days', function () {
+       expect(gd.generateDay(24)).to.eql('дня');
+    });
     it('should throws exception if second parameter is null', function () {
        expect(gd.generateDay.bind(null)).to.throw(Error);
     });
